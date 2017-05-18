2979795c3a3c0e63327e982921fef391848e83c7
Raffi Hovagimian <rhovagimian@salesforce.com>
2017-05-17 22:19:10 -0700

    Add lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
+	// run ESLint
+	gulp.src(["js/**/*.js", "public/js/**/*.js"])
+		.pipe(eslint())
+		.pipe(eslint.format());
 	// Node source
 	gulp.src("js/**/*.js")
 		.pipe(babel())
