990969d891d0b3bc2938edece5dbc6fa84000191
Raffi Hovagimian <rhovagimian@salesforce.com>
2017-05-18 22:01:44 -0700

    chapter 4 exercise

---
+++ chapter4/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+//const eslint = require('gulp-eslint');
+
+gulp.task('default', function() {
+	// run ESLint
+	// gulp.src(["js/**/*.js", "public/js/**/*.js"])
+	// 	.pipe(eslint())
+	// 	.pipe(eslint.format());
+	// Node source
+	gulp.src("js/**/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("dist"));
+
+	// Browser source
+	gulp.src("public/js/**/*.js")
+		.pipe(babel())
+		.pipe(gulp.dest("public/dist"));
+});
